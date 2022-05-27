a81ef86e5bcec625b6276cf754fe25e5877b4ce1
Paweł Żurawka <pzurawka@gmail.com>
2022-05-27 16:16:02 +0200

    Finish desktop menu.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaBars } from 'react-icons/fa';
-import { MobileIcon, Nav, NavbarContainer, NavItem, NavLinks, NavLogo, NavMenu } from './NavbarElements';
+import { MobileIcon, Nav, NavbarContainer, NavBtn, NavBtnLink, NavItem, NavLinks, NavLogo, NavMenu } from './NavbarElements';
 
 export const Navbar = () => {
   return (
@@ -13,9 +13,21 @@ export const Navbar = () => {
           </MobileIcon>
           <NavMenu>
             <NavItem>
-              <NavLinks to="about">About</NavLinks>
+              <NavLinks to="about">O mnie</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="stack">Technologie</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="projects">Projekty</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="contact">Kontakt</NavLinks>
             </NavItem>
           </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/send">Wyślij</NavBtnLink>
+          </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
