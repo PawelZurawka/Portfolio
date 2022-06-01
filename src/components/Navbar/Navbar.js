1ab57c46723c16ed0b59c2e2635f36ede1917402
Paweł Żurawka <pzurawka@gmail.com>
2022-06-01 14:25:21 +0200

    Add scrollToTop at top logo element.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { FaBars } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
+import { animateScroll as scroll } from 'react-scroll';
 import { MobileIcon, Nav, NavbarContainer, NavBtn, NavBtnLink, NavItem, NavLinks, NavLogo, NavMenu } from './NavbarElements';
 
 export const Navbar = ({ toggle }) => {
@@ -18,12 +19,20 @@ export const Navbar = ({ toggle }) => {
     window.addEventListener('scroll', changeNav);
   }, []);
 
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
         <Nav scrollNav={scrollNav}>
           <NavbarContainer>
-            <NavLogo to="/">logo</NavLogo>
+            <NavLogo
+              to="/"
+              onClick={toggleHome}>
+              logo
+            </NavLogo>
             <MobileIcon onClick={toggle}>
               <FaBars />
             </MobileIcon>
