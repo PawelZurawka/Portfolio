8da28b99426c5e49ddeffeb52ae7fc35a1d676d2
Paweł Żurawka <pzurawka@gmail.com>
2022-06-01 14:19:25 +0200

    Add scrollNav transparent effect and IconContext.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,38 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaBars } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
 import { MobileIcon, Nav, NavbarContainer, NavBtn, NavBtnLink, NavItem, NavLinks, NavLogo, NavMenu } from './NavbarElements';
 
 export const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeNav);
+  }, []);
+
   return (
     <>
-      <Nav>
-        <NavbarContainer>
-          <NavLogo to="/">logo</NavLogo>
-          <MobileIcon onClick={toggle}>
-            <FaBars />
-          </MobileIcon>
-          <NavMenu>
-            <NavItem>
-              <NavLinks to="/hero">Start</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="/stack">Umiejętności</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="/projects">Projekty</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="/skills">Skills</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="/contact">Kontakt</NavLinks>
-            </NavItem>
-          </NavMenu>
-          <NavBtn>
-            <NavBtnLink to="/send">Wyślij</NavBtnLink>
-          </NavBtn>
-        </NavbarContainer>
-      </Nav>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <Nav scrollNav={scrollNav}>
+          <NavbarContainer>
+            <NavLogo to="/">logo</NavLogo>
+            <MobileIcon onClick={toggle}>
+              <FaBars />
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLinks to="/hero">Start</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/stack">Umiejętności</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/projects">Projekty</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/skills">Skills</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/contact">Kontakt</NavLinks>
+              </NavItem>
+            </NavMenu>
+            <NavBtn>
+              <NavBtnLink to="/send">Wyślij</NavBtnLink>
+            </NavBtn>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
     </>
   );
 };
